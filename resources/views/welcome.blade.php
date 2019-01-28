bdb99f8704bb9d96451395b0540de67bf9eae344
Vlad <vlad@nesutulsa.ru>
2019-01-28 16:56:46 +0300

    Change home text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   <h2>Hello, Laravel!</h2>
+                   <h2>Hello world!</h2>
                 </div>
 
                 <div class="links">
