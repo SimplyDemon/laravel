a1dd59c555419d1a208a9c4bf6bc3e9b1bf1d07a
Vlad <vlad@nesutulsa.ru>
2019-01-28 16:53:28 +0300

    change text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   <h2>Hello, Laravel!</h2>
                 </div>
 
                 <div class="links">
